0a48193ae4761ddf25f8be80b37aa3a544b9110a
MovinduLochana <www.movindu@gmail.com>
2025-08-15 10:53:27 +0530

    Rest Test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
 
-export default function Home() {
+
+export default async function Home() {
+
+  const res = await fetch("http://localhost:8080/api/test/hello");
+  const data = await res.text()
+
+  console.log("Data fetched from API:", data);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     
+      <h1>{data}</h1>
     </div>
   );
 }
